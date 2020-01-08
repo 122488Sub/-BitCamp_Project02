c0ff95500cd66476af7fcc8be24438b459f6c97e
ru251 <ru251@kakao.com>
2020-01-08 12:08:31 +0900

    혜성
    컨트롤러생성

--- Code/src/com/bitbang/model/controller/PagingCommunity.java
+++ Code/src/com/bitbang/model/controller/PagingCommunity.java
@@ -16,4 +16,89 @@ public class PagingCommunity {
 	
 	private int beginPage = 0; //현재 블록의 시작 페이지 번호
 	private int endPage = 0; //현재 블록의 끝 페이지 번호
+	
+	public void setTotalPage() {
+		totalPage = totalRecord / numPerPage;
+		if (totalRecord % numPerPage > 0) totalPage++;
+	}
+	
+	public PagingCommunity() {
+		super();
+	}
+	
+	public int getNowPage() {
+		return nowPage;
+	}
+	public void setNowPage(int nowPage) {
+		this.nowPage = nowPage;
+	}
+	public int getNowBlock() {
+		return nowBlock;
+	}
+	public void setNowBlock(int nowBlock) {
+		this.nowBlock = nowBlock;
+	}
+	public int getNumPerPage() {
+		return numPerPage;
+	}
+	public void setNumPerPage(int numPerPage) {
+		this.numPerPage = numPerPage;
+	}
+	public int getPagePerBlock() {
+		return pagePerBlock;
+	}
+	public void setPagePerBlock(int pagePerBlock) {
+		this.pagePerBlock = pagePerBlock;
+	}
+	public int getTotalRecord() {
+		return totalRecord;
+	}
+	public void setTotalRecord(int totalRecord) {
+		this.totalRecord = totalRecord;
+	}
+	public int getTotalPage() {
+		return totalPage;
+	}
+	public void setTotalPage(int totalPage) {
+		this.totalPage = totalPage;
+	}
+	public int getTotalBlock() {
+		return totalBlock;
+	}
+	public void setTotalBlock(int totalBlock) {
+		this.totalBlock = totalBlock;
+	}
+	public int getBegin() {
+		return begin;
+	}
+	public void setBegin(int begin) {
+		this.begin = begin;
+	}
+	public int getEnd() {
+		return end;
+	}
+	public void setEnd(int end) {
+		this.end = end;
+	}
+	public int getBeginPage() {
+		return beginPage;
+	}
+	public void setBeginPage(int beginPage) {
+		this.beginPage = beginPage;
+	}
+	public int getEndPage() {
+		return endPage;
+	}
+	public void setEndPage(int endPage) {
+		this.endPage = endPage;
+	}
+	@Override
+	public String toString() {
+		return "PagingCommunity [nowPage=" + nowPage + ", nowBlock=" + nowBlock + ", numPerPage=" + numPerPage
+				+ ", pagePerBlock=" + pagePerBlock + ", totalRecord=" + totalRecord + ", totalPage=" + totalPage
+				+ ", totalBlock=" + totalBlock + ", begin=" + begin + ", end=" + end + ", beginPage=" + beginPage
+				+ ", endPage=" + endPage + "]";
+	}
+	
+	
 }
