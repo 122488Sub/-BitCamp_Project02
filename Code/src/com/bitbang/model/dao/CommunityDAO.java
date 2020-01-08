436fc2538ccb9ca8935bafdfb90c20fa3f4d6f89
ru251 <ru251@kakao.com>
2020-01-08 14:47:13 +0900

    내꺼

--- Code/src/com/bitbang/model/dao/CommunityDAO.java
+++ Code/src/com/bitbang/model/dao/CommunityDAO.java
@@ -2,14 +2,19 @@ package com.bitbang.model.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+
 import com.bitbang.model.vo.CommunityVO;
+import com.bitbang.mybatis.DBService;
 
 public class CommunityDAO implements DAO {
 
 	@Override
 	public List<CommunityVO> selectListPage() {
-		
-		return null;
+		SqlSession ss = DBService.getFactory().openSession();
+		List<CommunityVO> list = ss.selectList("");
+		ss.close();
+		return list;
 	}
 
 	@Override
