3d3f600b4c48de3c2a18bb4c405c9d45f6535a3c
fennecfox <qkrwlgh91@gmail.com>
2020-01-14 13:52:49 +0900

    지호 1.14 오전

--- Code/src/com/bitbang/model/vo/BNB_PAGEVO.java
+++ Code/src/com/bitbang/model/vo/BNB_PAGEVO.java
@@ -4,24 +4,25 @@ public class BNB_PAGEVO {
 	private int nowPage = 1; //현재페이지
 	private int nowBlock = 1; //현재 블록(페이지 담는 단위)
 	
-	private int numPerPage = 5; // 하나의 페이지에 표시할 게시글 수
-	private int pagePerBlock = 3; // 블록당 표시하는 페이지 갯수
+	private int numPerPage = 5; //하나의 페이지에 표시할 게시글 수
+	private int pagePerBlock = 3; //블록당 표시하는 페이지 갯수
 	
-	private int totalRecord = 0; //총 게시물 갯수
-	private int totalPage = 0; // 전체 페이지 갯수
+	private int totalRecord = 0; //총 게시물 갯수(원본 게시글 수)
+	private int totalPage = 0; //전체 페이지 갯수
 	private int totalBlock = 0; //전체 블록 갯수
 	
-	private int begin = 0; //현재 블록의 시작 페이지 번호
-	private int end = 0; //현재 페이지상의 마지막 글 번호
+	private int begin = 0; //현재 페이지상의 시작 글번호
+	private int end = 0; //현재 페이지상의 마지막 글번호
 	
 	private int beginPage = 0; //현재 블록의 시작 페이지 번호
 	private int endPage = 0; //현재 블록의 끝 페이지 번호
 	
 	//전체 페이지 갯수 구하기
-	// totalRecord 값을 페이지당 표시할 글의 개수 값을 나누고, 나머지가 있으면 페이지 하나 더 추가
+	//totalRecord 값을 페이지당 표시할 글의 개수 값을 나누고,
+	//나머지가 있으면 페이지 하나 더 추가
 	public void setTotalPage() {
 		totalPage = totalRecord / numPerPage;
-		if(totalRecord % numPerPage > 0) totalPage++;
+		if (totalRecord % numPerPage > 0) totalPage++;
 	}
 	
 	public int getNowPage() {
@@ -78,25 +79,29 @@ public class BNB_PAGEVO {
 	public void setEnd(int end) {
 		this.end = end;
 	}
+
 	public int getBeginPage() {
 		return beginPage;
 	}
+
 	public void setBeginPage(int beginPage) {
 		this.beginPage = beginPage;
 	}
+
 	public int getEndPage() {
 		return endPage;
 	}
+
 	public void setEndPage(int endPage) {
 		this.endPage = endPage;
 	}
+
 	@Override
 	public String toString() {
-		return "BNB_PAGEVO [nowPage=" + nowPage + ", nowBlock=" + nowBlock + ", numPerPage=" + numPerPage
+		return "Paging [nowPage=" + nowPage + ", nowBlock=" + nowBlock + ", numPerPage=" + numPerPage
 				+ ", pagePerBlock=" + pagePerBlock + ", totalRecord=" + totalRecord + ", totalPage=" + totalPage
 				+ ", totalBlock=" + totalBlock + ", begin=" + begin + ", end=" + end + ", beginPage=" + beginPage
 				+ ", endPage=" + endPage + "]";
 	}
-	
 	
 }
